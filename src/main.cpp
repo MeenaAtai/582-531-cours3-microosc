52a43173181e42e28965065e9fe34f224a8a6dec
Meena Atai <2397995@cmontmorency.qc.ca>
2025-09-09 11:12:57 -0400

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#define MA_BROCHE_BOUTON 39
+
+#include <MicroOscSlip.h>
+
+MicroOscSlip<128> monOsc(& Serial);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+}
+
+void loop() {
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  //Serial.println(maLectureBouton);
+  monOsc.sendInt("/bouton", maLectureBouton);
+  delay(100);
+}
